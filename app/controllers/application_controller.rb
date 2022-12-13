b650b4fea0776efb63e969897cf1bb3a06c50857
tabithatrubey <tabithatrubey@outlook.com>
2022-12-13 12:46:56 -0800

    Seriously had one too many ends that threw off my entire method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+    # disable Authenticity token to be able to create a new instance of animal in POSTMAN
+    skip_before_action :verify_authenticity_token
 end
---
+++ db/migrate/20221213193148_drop_animal.rb
@@ -0,0 +1,4 @@
+class DropAnimal < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
